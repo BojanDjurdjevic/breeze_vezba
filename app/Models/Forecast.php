66e80a7193f1b149941761b586f10806de320559
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-09 21:02:15 +0100

    Forecast model -> fillable fields added

--- app/Models/Forecast.php
+++ app/Models/Forecast.php
@@ -9,7 +9,8 @@ class Forecast extends Model
     protected $table = 'forecasts';
 
     protected $fillable = [
-        'city_id', 'temperature', 'date'
+        'city_id', 'temperature', 'date',
+        'weather_type', 'probability'
     ];
 
     public function city()
