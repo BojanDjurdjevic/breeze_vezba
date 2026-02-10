b3781f2ca1766947c1ed42eb9f2ee7378c109f04
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-10 20:08:53 +0100

    Welcome blade -> search added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,31 @@
 <x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class=" shadow-sm sm:rounded-lg flex justify-center">
+                <form
+                    action="/admin/update-city" method="GET" 
+                    class="bg-white w-full md:max-w-md sm:max-w-sm flex items-center flex-col align-center rounded-xl shadow-xl">
+                    @if ($errors->any())
+                        <p class="text-red-600"><b>Greška:{{ $errors->first() }}</b></p>
+                    @endif
+                    @csrf
+                    <h3 class="text-center text-indigo-700">Pronađite grad</h3>
+                    <select 
+                        name="city_id" id="" class="p-2 rounded-xl m-3 
+                        hover:bg-gray-300 focus:border-indigo-700 w-72 self-center"
+                        >
+                        @foreach ($cities as $city)
+                            <option value="{{ $city->id }}">{{ $city->name }}</option>
+                        @endforeach
+                    </select>
+                    <button type="submit" 
+                        class="p-2 m-3 text-white bg-indigo-700 rounded-xl w-36 self-center"
+                    >Pronađi</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    
     <div class="w-full max-w-4xl mx-auto mt-6 bg-white rounded-xl shadow border border-indigo-200 overflow-x-auto mb-6"
         x-data="{open: false}"
     >
@@ -14,11 +41,11 @@
                 @foreach ($cities as $c)
                     <tr class="border-t hover:bg-gray-50 transition">
                         <td class="p-3 font-medium">
-                            {{ $c->city->name }}
+                            {{ $c->name }}
                         </td>
 
                         <td class="p-3">
-                            {{ $c->temp }} °C
+                            {{ $c->weather->temp }} °C
                         </td>
                     </tr>
                 @endforeach
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,12 @@ use App\Http\Controllers\CityController;
 use App\Http\Controllers\ForecastController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\CheckAdmin;
+use App\Models\City;
 use App\Models\CityWeatherModel;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $cities = CityWeatherModel::all();
+    $cities = City::all();
     return view('welcome', compact('cities'));
 });
 
