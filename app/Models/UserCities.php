f59195ed4c02356889944300c76b33e85710dcac
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-11 14:22:36 +0100

    UserCity model & migration -> not migrated still

---
+++ app/Models/UserCities.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserCities extends Model
+{
+    protected $table = 'user_city';
+
+    protected $fillable = [
+        'city_id', 'user_id'
+    ];
+
+    public function users() 
+    {
+        return $this->hasbelongsToMany(User::class);
+    }
+
+    public function city() 
+    {
+        return $this->belongsTo(City::class);
+    }
+}
