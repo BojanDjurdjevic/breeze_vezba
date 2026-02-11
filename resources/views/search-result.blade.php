d63046212b10ea8feff753b41513318da42dc033
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-11 18:10:10 +0100

    Heart icon added

--- resources/views/search-result.blade.php
+++ resources/views/search-result.blade.php
@@ -6,10 +6,17 @@
                 $icon = App\Http\ForecastHelper::getIcon($city->todaysForecast->weather_type);
             @endphp
             <button
-            class="p-3 m-1 bg-indigo-500 text-white rounded-lg hover:bg-indigo-700"
-            ><a href="{{ route('all-city-forecasts', ['city' => $city->name])  }}">
-            <i class="mdi {{ $icon }}"></i>
-            {{ $city->name }}</a></button>
+            class="p-3 m-1 bg-indigo-500 text-white rounded-lg hover:bg-indigo-700
+                    flex items-center justify-between"
+            >
+                <a href=""><i class="mdi mdi-heart text-white"></i></a>
+                
+                <a href="{{ route('all-city-forecasts', ['city' => $city->name])  }}">
+                    <i class="mdi {{ $icon }}"></i>
+                    {{ $city->name }}
+                </a>
+            </button>
         @endforeach
     </div>
 </x-app-layout>
+
